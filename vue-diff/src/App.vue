4eccfc6c9ef07eb3a09ae9f3ed84ff9abc5e3207
kimsolee <ahhasolee95@gmail.com>
2025-02-09 19:08:51 +0900

    feat: git style diff viewer 적용 test

---
+++ vue-diff/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+
+  <div>
+  
+    <DiffViewTest />
+  </div>
+
+<!-- 
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import DiffViewTest from './components/DiffViewTest.vue';
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    DiffViewTest
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-diff/src/components/DiffViewTest.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="diff-container">
+    ffff
+    <Diff
+      :mode="'split'"
+      :theme="'light'"
+      :prev="'old version\nline 2\nline 3'"
+      :current="'old version\nline 2 changed\nline 3'"
+    />
+  </div>
+</template>
+
+<script>
+import { Diff } from "vue-diff";
+import "vue-diff/dist/index.css";
+
+export default {
+  components: {
+    Diff,
+  },
+};
+</script>
+
+<style>
+/* .diff-container {
+  max-width: 100%;
+  overflow-x: auto;
+  min-height: 200px;
+} */
+</style>
